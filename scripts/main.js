f3365fe060e9c535617ea7badebcdc2d4b5cb580
KTZHANG <kennyz620@hotmail.com>
2023-07-06 18:09:03 -0700

    Modified boilerplate to allow for collaborative work.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,55 @@
-console.log("Hello world!\n")
+// For those brave enough to touch Javascript, here is a skill testing question:
+// How do linked lists work?
+
+// As with CSS and HTML, touch only your own section to avoid merge conflicts.
+
+
+// Script Section 1
+
+console.log("Hello world!\n");
+
+
+
+
+// End of Section
+
+
+// Script Section 2
+
+
+
+
+
+
+// End of Section
+
+
+
+// Script Section 3
+
+
+
+
+
+
+// End of Section
+
+
+// Script Section 4
+
+
+
+
+
+
+// End of Section
+
+
+// Script Section 5
+
+
+
+
+
+
+// End of Section
