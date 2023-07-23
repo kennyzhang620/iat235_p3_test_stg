ef952b8a12021b6dd38850fa6ebd16a89b1de79b
DESKTOP-G0QRCDB\Win 10 Pro <cda79@sfu.ca>
2023-07-22 23:11:14 -0700

    (Section 6) FAQ Progress - collapsible questions & BG

--- scripts/main.js
+++ scripts/main.js
@@ -53,3 +53,20 @@ console.log("Hello world!\n");
 
 
 // End of Section
+
+// Script Section 6
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+//End of Section
