180b7bd7d38e78a03aa91de1b6386b1f1d994879
SING\df030 <wxd@sfu.ca>
2023-07-26 05:12:04 -0700

    Finished styling and animating the first section of the ticket page. Including both 18 and 19.

--- scripts/interactivable_seat_sections.js
+++ scripts/interactivable_seat_sections.js
@@ -6,25 +6,57 @@ function updateImage(src) {
     }
 }
 
+// ---------------------------------------Mouse over--------------------------------
 document.getElementById("backBalcony").addEventListener("mouseover", function(event){
   event.preventDefault();
-  updateImage("images/ticket/BackBalcony.png");
+  updateImage("images/ticket/new/Back_Balcony.png");
 });
 
 document.getElementById("frontBalcony").addEventListener("mouseover", function(event){
   event.preventDefault();
-  updateImage("images/ticket/FrontBalcony.png");
+  updateImage("images/ticket/new/Front_Balcony.png");
 });
 
 document.getElementById("backOrchestra").addEventListener("mouseover", function(event){
   event.preventDefault();
-  updateImage("images/ticket/BackOrchestra.png");
+  updateImage("images/ticket/new/Back_Orchestra.png");
 });
 
 document.getElementById("frontOrchestra").addEventListener("mouseover", function(event){
   event.preventDefault();
-  updateImage("images/ticket/FrontOrchestra.png");
+  updateImage("images/ticket/new/Front_Orchestra.png");
 });
+// ---------------------------------------Mouse out-------------------------------------
+
+document.getElementById("backBalcony").addEventListener("mouseout", function(event){
+  event.preventDefault();
+  if (enableHover) {
+    document.getElementById("seatingImage").src = "images/ticket/new/empty.png";
+  }
+});
+
+document.getElementById("frontBalcony").addEventListener("mouseout", function(event){
+  event.preventDefault();
+  if (enableHover) {
+    document.getElementById("seatingImage").src = "images/ticket/new/empty.png";
+  }
+});
+
+document.getElementById("backOrchestra").addEventListener("mouseout", function(event){
+  event.preventDefault();
+  if (enableHover) {
+    document.getElementById("seatingImage").src = "images/ticket/new/empty.png";
+  }
+});
+
+document.getElementById("frontOrchestra").addEventListener("mouseout", function(event){
+  event.preventDefault();
+  if (enableHover) {
+    document.getElementById("seatingImage").src = "images/ticket/new/empty.png";
+  }
+});
+
+// -------------------------------------Click---------------------------------------
 
 document.getElementById("backBalcony").addEventListener("click", function(event){
   event.preventDefault();
@@ -55,3 +87,4 @@ document.getElementById("returnLink").addEventListener("click", function(event){
   enableHover = true;
   document.getElementById("seatingImage").src = "images/ticket/BackBalcony.png";
 });
+// -------------------------------------------------------------------------------
