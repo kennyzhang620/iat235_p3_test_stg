5963f686cabf82485e805f1890b67013e94d38b8
SING\df030 <wxd@sfu.ca>
2023-07-27 05:47:35 -0700

    Finalized the section page, working on the seat map page. added URL search so the section and seat map bridge according to the section user choose.

--- scripts/vfx_ticket_section.js
+++ scripts/vfx_ticket_section.js
@@ -58,29 +58,59 @@ document.getElementById("frontOrchestra").addEventListener("mouseout", function(
 
 // -------------------------------------Click---------------------------------------
 
-document.getElementById("backBalcony").addEventListener("click", function(event){
-  event.preventDefault();
-  document.getElementById("seatingImage").src = "images/ticket/Hall_seating_backBalcony_1698x1312.png";
-  enableHover = false;
+// document.getElementById("backBalcony").addEventListener("click", function(event){
+//   event.preventDefault();
+//   document.getElementById("seatingImage").src = "images/ticket/Hall_seating_backBalcony_1698x1312.png";
+//   enableHover = false;
+// });
+
+// document.getElementById("frontBalcony").addEventListener("click", function(event){
+//   event.preventDefault();
+//   document.getElementById("seatingImage").src = "images/ticket/Hall_seating_frontBalcony_1698x1158.png";
+//   enableHover = false;
+// });
+
+// document.getElementById("backOrchestra").addEventListener("click", function(event){
+//   event.preventDefault();
+//   document.getElementById("seatingImage").src = "images/ticket/Hall_seating_backOrchestra_1698x965.png";
+//   enableHover = false;
+// });
+
+// document.getElementById("frontOrchestra").addEventListener("click", function(event){
+//   event.preventDefault();
+//   document.getElementById("seatingImage").src = "images/ticket/Hall_seating_frontOrchestra_1698x1099.png";
+//   enableHover = false;
+// });
+
+
+//--------------------------------------Background parallox effect------------------------- 
+
+// reference: https://css-tricks.com/moving-backgrounds-with-mouse-position/
+
+var minWidth = 60 * 16; // Convert rem to px
+
+document.body.addEventListener('mousemove', function(event) {
+  if (window.innerWidth > minWidth) {
+    var x = event.clientX / window.innerWidth * 5 - 2.5;  // to get values from -5% to +5%
+    var y = event.clientY / window.innerHeight * 5 - 2.5; // to get values from -5% to +5%
+    document.getElementById('baseImage').style.transform = "translate(" + x + "%, " + y + "%)";
+    document.getElementById('seatingImage').style.transform = "translate(" + x + "%, " + y + "%)";
+  }
 });
 
-document.getElementById("frontBalcony").addEventListener("click", function(event){
-  event.preventDefault();
-  document.getElementById("seatingImage").src = "images/ticket/Hall_seating_frontBalcony_1698x1158.png";
-  enableHover = false;
+document.addEventListener('mousemove', function(event) {
+  if (window.innerWidth > minWidth) {
+    var seatSections = document.querySelector('.seat_sections');
+    var x = event.clientX / window.innerWidth * 5 - 2.5;
+    var y = event.clientY / window.innerHeight * 5 - 2.5;
+    seatSections.style.transform = 'translate(' + (-50 + x) + '%,' + (-50 + y) + '%)';
+  }
 });
 
-document.getElementById("backOrchestra").addEventListener("click", function(event){
-  event.preventDefault();
-  document.getElementById("seatingImage").src = "images/ticket/Hall_seating_backOrchestra_1698x965.png";
-  enableHover = false;
-});
 
-document.getElementById("frontOrchestra").addEventListener("click", function(event){
-  event.preventDefault();
-  document.getElementById("seatingImage").src = "images/ticket/Hall_seating_frontOrchestra_1698x1099.png";
-  enableHover = false;
-});
+
+
+
 
 // document.getElementById("returnLink").addEventListener("click", function(event){
 //   event.preventDefault();
